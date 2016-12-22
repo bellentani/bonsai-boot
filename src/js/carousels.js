b18f302087db5219faa6f8baee32136ba0987b44
Poe Bellentani <contato@fernandob.com.br>
2016-12-22 16:31:21 -0200

    Estrutura inicial do Owl Carousel 2 inserida no projeto.

---
+++ src/js/carousels.js
@@ -0,0 +1,19 @@
+$(function () {
+  //
+  $('.owl-fullbanner').addClass('owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items: 1
+        }
+    }
+  });
+})
